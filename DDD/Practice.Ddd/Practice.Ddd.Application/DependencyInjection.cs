8979a369761f3ceb4dff2f1a0f5d5c63e22b0497
Shogo Matsumoto <shogo.matsumoto@avanade.com>
2024-03-20 09:01:25 +0900

    Initial push

---
+++ DDD/Practice.Ddd/Practice.Ddd.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Practice.Ddd.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = typeof(Practice.Ddd.Application.DependencyInjection).Assembly;
+        services.AddMediatR(configuration => 
+            configuration.RegisterServicesFromAssemblies(assembly));
+
+        services.AddValidatorsFromAssembly(assembly);
+
+        return services;
+    }
+}
---
+++ DDD/Practice.Ddd/Practice.Ddd.Domain/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Practice.Ddd.Domain;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDomain(this IServiceCollection services)
+    {
+        return services;
+    }
+}
